bf984b59850985e5a63447ae9583fb4631912192
Nadege Moisan <namoisan@c1r5p8.42quebec.com>
2024-11-20 15:37:05 -0500

    channel and user hpp in progress. Try to find the best way to structure

--- src/channel.hpp
+++ src/channel.hpp
@@ -4,15 +4,28 @@
 # include <iostream>
 # include "user.hpp"
 # include <vector>
+# include <map>
 
 class Channel
 {
 	private:
 		std::string _name;
-		std::vector<User> _users;
+		std::string _topic;
+		std::vector<User*> _members;
+		std::vector<User*> _operators; //admin
+		std::map<char, std::string> _modes;
+		std::string _password;
+		int _memberLimit;
 	public:
 		Channel(std::string name);
 		~Channel();
+
+		void	addMember(User *user);
+		void	removeMember(User *user);
+		void	setName(std::string name, User *operators);
+		void	setTopic(const std::string &topic, User *user);
+		std::string getTopic();
+
 };
 
 #endif
--- src/user.hpp
+++ src/user.hpp
@@ -8,7 +8,7 @@ class User
 	private:
 		std::string _username;
 		std::string _nickname;
-		int fd;
+		//à voir pour aussi mettre la liste des channels en map pour voir si il est admin ou pas. Voir si c'est pertinent car deja visible dans le channel
 	public:
 		User(std::string username, std::string nickname, int fd);
 		User(int fd);
