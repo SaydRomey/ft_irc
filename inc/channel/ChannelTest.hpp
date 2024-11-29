89e4ed90646293302a515e02a3faf13fd918473c
Cederic Dumais <cdumais@c1r3p7.42quebec.com>
2024-11-28 22:33:59 -0500

    Merged channel and user files from 'struct' branch to 'main' branch

---
+++ inc/channel/ChannelTest.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include "Channel.hpp"
+#include "User.hpp"
+
+class ChannelTest
+{
+	private:
+		void setModeTest();
+		void addRemoveTests();
+		void commandsOpTest();
+	public:
+		void runTests();
+};
---
+++ src/channel/ChannelTest.cpp
@@ -0,0 +1,28 @@
+#include "Channel.hpp"
+
+void setModeTest()
+{
+
+}
+
+void addRemoveTests()
+{
+	std::cout << "---------addRemove Tests---------" << std::endl;
+	std::vector<User> users;
+	users.push_back(User("Naddy", "Nad"));
+	Channel naruto("Naruto", users.find(""));
+	User anboisve("anboisve", "antoine");
+	naruto.addMember(anboisve);
+}
+
+void commandsOpTest()
+{
+
+}
+
+void runTests()
+{
+	addRemoveTests();
+	setModeTest();
+	commandsOpTest();
+}
