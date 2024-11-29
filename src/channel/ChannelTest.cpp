4a00cb8d6dbb4a14eb5dd2d76f126c25c3141411
Sayd Romey <sayd@null.net>
2024-11-29 01:29:46 -0500

    prep for server merge

--- src/channel/ChannelTest.cpp
+++ src/channel/ChannelTest.cpp
@@ -1,28 +1,28 @@
-#include "Channel.hpp"
+// #include "Channel.hpp"
 
-void setModeTest()
-{
+// void setModeTest()
+// {
 
-}
+// }
 
-void addRemoveTests()
-{
-	std::cout << "---------addRemove Tests---------" << std::endl;
-	std::vector<User> users;
-	users.push_back(User("Naddy", "Nad"));
-	Channel naruto("Naruto", users.find(""));
-	User anboisve("anboisve", "antoine");
-	naruto.addMember(anboisve);
-}
+// void addRemoveTests()
+// {
+// 	std::cout << "---------addRemove Tests---------" << std::endl;
+// 	std::vector<User> users;
+// 	users.push_back(User("Naddy", "Nad"));
+// 	Channel naruto("Naruto", users.find(""));
+// 	User anboisve("anboisve", "antoine");
+// 	naruto.addMember(anboisve);
+// }
 
-void commandsOpTest()
-{
+// void commandsOpTest()
+// {
 
-}
+// }
 
-void runTests()
-{
-	addRemoveTests();
-	setModeTest();
-	commandsOpTest();
-}
+// void runTests()
+// {
+// 	addRemoveTests();
+// 	setModeTest();
+// 	commandsOpTest();
+// }
