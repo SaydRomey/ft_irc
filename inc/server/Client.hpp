812fc52565e23cbd22ad74e30bd2b3865733d6bf
Sayd Romey <sayd@null.net>
2024-12-12 09:33:48 -0500

    merged server and user files from server branch to main

--- inc/server/Client.hpp
+++ inc/server/Client.hpp
@@ -1,54 +1,54 @@
-// #ifndef CLIENT_HPP
-// # define CLIENT_HPP
-// # include <string>
-// # include <stdexcept>
-// # include <sys/socket.h>
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+# include <string>
+# include <stdexcept>
+# include <sys/socket.h>
 
-// # define RESET		"\033[0m"
-// # define BOLD		"\033[1m"
-// # define ITALIC		"\033[3m"
-// # define UNDERLINE	"\033[4m"
-// # define RED		"\033[31m"
-// # define GREEN		"\033[32m"
-// # define YELLOW		"\033[33m"
-// # define BLUE		"\033[34m"
-// # define PURPLE		"\033[35m"
-// # define CYAN		"\033[36m"
-// # define ORANGE		"\033[38;5;208m"
-// # define GRAYTALIC	"\033[3;90m"
+# define RESET		"\033[0m"
+# define BOLD		"\033[1m"
+# define ITALIC		"\033[3m"
+# define UNDERLINE	"\033[4m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define PURPLE		"\033[35m"
+# define CYAN		"\033[36m"
+# define ORANGE		"\033[38;5;208m"
+# define GRAYTALIC	"\033[3;90m"
 
-// namespace ft
-// {
+namespace ft
+{
 
-// class Client
-// {
-// public:
-// 	Client(void); // Only for map element init, sets fd to -1
-// 	Client(int fd);
-// 	~Client();
-// 	Client& operator=(const Client& that);
+class Client
+{
+public:
+	Client(void); // Only for map element init, sets fd to -1
+	Client(int fd);
+	~Client();
+	Client& operator=(const Client& that);
 
-// 	void	setNickname(const std::string& nickname);
-// 	void	setUsername(const std::string& username);
-// 	void	setRegistered(void); // Throws logic_error if nickname or username isn't set
-// 	void	setAuthenticated(bool auth);
+	void	setNickname(const std::string& nickname);
+	void	setUsername(const std::string& username);
+	void	setRegistered(void); // Throws logic_error if nickname or username isn't set
+	void	setAuthenticated(bool auth);
 
-// 	const std::string&	getNickname(void) const;
-// 	const std::string&	getUsername(void) const;
-// 	bool				isRegistered(void) const;
-// 	bool				isAuthenticated(void) const;
+	const std::string&	getNickname(void) const;
+	const std::string&	getUsername(void) const;
+	bool				isRegistered(void) const;
+	bool				isAuthenticated(void) const;
 
-// 	// TODO: move the following methods from Server into Client
-// 	void	sendMessage(const std::string& msg);
+	// TODO: move the following methods from Server into Client
+	void	sendMessage(const std::string& msg);
 
-// private:
-// 	int			_fd;
-// 	std::string	_username;
-// 	std::string	_nickname;
-// 	bool		_registered;
-// 	bool		_authenticated;
-// };
+private:
+	int			_fd;
+	std::string	_username;
+	std::string	_nickname;
+	bool		_registered;
+	bool		_authenticated;
+};
 
-// }
+}
 
-// #endif
+#endif
