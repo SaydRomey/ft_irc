64bd357404ce65377a11843a4298a354fe65af99
Cederic Dumais <cdumais@c1r3p9.42quebec.com>
2024-12-21 00:01:40 -0500

    commented server, user and channel code, to work on parsing

--- irc/server/User.hpp
+++ irc/server/User.hpp
@@ -1,53 +1,53 @@
-#ifndef USER_HPP
-# define USER_HPP
+// #ifndef USER_HPP
+// # define USER_HPP
 
-# include <iostream>
-# include <queue>
-# include <set>
-# include <string>
+// # include <iostream>
+// # include <queue>
+// # include <set>
+// # include <string>
 
-# define PERM_PASS 0x00f
-# define PERM_NICK 0x0f0
-# define PERM_USER 0xf00
-# define PERM_ALL  0xfff
+// # define PERM_PASS 0x00f
+// # define PERM_NICK 0x0f0
+// # define PERM_USER 0xf00
+// # define PERM_ALL  0xfff
 
-class User
-{
-	private:
-		int			_fd;
-		std::string	_username;
-		std::string	_nickname;
-		short		_perms;
+// class User
+// {
+// 	private:
+// 		int			_fd;
+// 		std::string	_username;
+// 		std::string	_nickname;
+// 		short		_perms;
 
-		std::string				_msgBuffer;
-		std::queue<std::string>	_pending;
-		std::set<std::string>	_channels;
-	public:
-		User(void);
-		User(int fd);
-		User& operator=(const User& other);
+// 		std::string				_msgBuffer;
+// 		std::queue<std::string>	_pending;
+// 		std::set<std::string>	_channels;
+// 	public:
+// 		User(void);
+// 		User(int fd);
+// 		User& operator=(const User& other);
 
-		void setNickname(const std::string& nickname);
-		void setUsername(const std::string& username);
-		void setFd(int fd);
-		const std::string& getNickname(void) const;
-		const std::string& getUsername(void) const;
-		int getFd(void) const;
+// 		void setNickname(const std::string& nickname);
+// 		void setUsername(const std::string& username);
+// 		void setFd(int fd);
+// 		const std::string& getNickname(void) const;
+// 		const std::string& getUsername(void) const;
+// 		int getFd(void) const;
 
-		short		getPerms(void) const;
-		void		setPass(bool valid);
+// 		short		getPerms(void) const;
+// 		void		setPass(bool valid);
 
-		void				addToMsgBuffer(const std::string& packet);
-		void				resetMsgBuffer(const std::string& msg);
-		const std::string	extractFromBuffer(void);
+// 		void				addToMsgBuffer(const std::string& packet);
+// 		void				resetMsgBuffer(const std::string& msg);
+// 		const std::string	extractFromBuffer(void);
 
-		size_t				pendingSize(void) const;
-		void				pendingPush(const std::string& msg);
-		const std::string	pendingPop(void);
+// 		size_t				pendingSize(void) const;
+// 		void				pendingPush(const std::string& msg);
+// 		const std::string	pendingPop(void);
 
-		bool	addChannel(const std::string& chan);
-		bool	removeChannel(const std::string& chan);
-		bool	inChannel(const std::string& chan);
-};
+// 		bool	addChannel(const std::string& chan);
+// 		bool	removeChannel(const std::string& chan);
+// 		bool	inChannel(const std::string& chan);
+// };
 
-#endif
+// #endif
