22f1d7908fc78bc2a15a112779b0f71e1f6c183d
Cederic Dumais <cdumais@c1r4p2.42quebec.com>
2025-01-17 18:24:54 -0500

    merged channel specific files from channel branch to parsing branch

--- irc/channel/ChannelManager.cpp
+++ irc/channel/ChannelManager.cpp
@@ -31,15 +31,14 @@ void ChannelManager::joinManager(User &sender, const Message &msg)
 
 void ChannelManager::partManager(User &sender, const Message &msg)
 {
-	std::vector<std::pair<std::string, std::string> > ChannelsAndKeys = msg.getChannelsAndKeys();
+	std::vector<std::string>  channelsVec = tokenize(msg.getParamsVec()[0], ',');
 	const std::string &reason = msg.getTrailing();
-	for (size_t i = 0; i < ChannelsAndKeys.size(); ++i)
+	for (size_t i = 0; i < channelsVec.size(); ++i)
 	{
-		const std::string &channelName = ChannelsAndKeys[i].first;
+		const std::string &channelName = channelsVec[i];
 		if (_channels.find(channelName) == _channels.end())
 		{
 			sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
-			// std::cout << ":server 403 " << sender.getNickname() << " " << channelName << " :No such channel" << std::endl;
 			continue;
 		}
 		_channels[channelName].removeMember(sender, reason);
@@ -51,103 +50,94 @@ void ChannelManager::partManager(User &sender, const Message &msg)
 void ChannelManager::inviteManager(User &sender, const Message &msg)
 {
 	// Extraire les paramètres : channel et utilisateur à inviter
-	std::vector<std::pair<std::string, std::string> > ChannelsAndKeys = msg.getChannelsAndKeys();
-	for (size_t i = 0; i < ChannelsAndKeys.size(); ++i)
+	std::string inviteeNickname = msg.getParamsVec()[0];
+	std::vector<std::string> channelsVec = tokenize(msg.getParamsVec()[1], ',');
+	for (size_t i = 0; i < channelsVec.size(); ++i)
 	{
-		std::string channelName = ChannelsAndKeys[i].first;
-		std::string inviteeNickname = ChannelsAndKeys[i].second;
+		std::string channelName = channelsVec[i];
 		if (_channels.find(channelName) == _channels.end())
 		{
 			sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
-			// std::cout << ":server 403 " << sender.getNickname() << " " << channelName << " :No such channel" << std::endl;
 			continue;
 		}
 		// Inviter l'utilisateur
-		// User& target = getUserByNickname(inviteeNickname); // Fonction à implémenter dans serveur?
-		User* target = _server.getUserByNickname(inviteeNickname); //
+		User* target = _server.getUserByNickname(inviteeNickname);
 		if (target == NULL) //ERR_NOSUCHNICK
 		{
 			sender.pendingPush(reply(ERR_NOSUCHNICK, sender.getNickname(), inviteeNickname));
-			// std::cout << ":server 401 " << sender.getNickname() << " " << inviteeNickname << " :No such nick" << std::endl;
 			continue;
 		}
-		_channels[channelName].invite(*target, sender); //
-		// _channels[channelName].invite(target, sender);
-
-		// Broadcast the invitation to the channel members
-		// std::cout << ":" << sender.getNickname() << " INVITE " << inviteeNickname << " " << channelName << std::endl;
-		// std::string	message = ":" + sender.getNickname() + " INVITE " + inviteeNickname + " " + channelName + "\r\n";
-		std::string	message = reply(RPL_INVITING, sender.getNickname(), inviteeNickname, channelName);
-		_channels[channelName].broadcast(sender, message); // already handled in Channel.invite() ?
+		_channels[channelName].invite(*target, sender);
 	}
 }
 
 void ChannelManager::kickManager(User &sender, const Message &msg)
 {
-	(void)sender;
-	(void)msg;
-	//voir pour envoyé un message par default si pas de raison envoyé
+	std::string channelName = msg.getParamsVec()[0];
+	std::vector<std::string> targetsNickname = tokenize(msg.getParamsVec()[1], ',');
+	const std::string &reason = msg.getTrailing();
 
-	// const std::string&	channelName = 
-	// const std::string&	targetNickname =
-	// const std::string&	reason =
-
-	// if (_channels.find(channelName) == _channels.end())
-	// {
-	// 	sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
-	// 	return ;
-	// }
-
-	// // retrieve the target user
-	// User*	target = _server.getUserByNickname(targetNickname);
-	// if (!target)
-	// {
-	// 	sender.pendingPush(reply(ERR_NOSUCHNICK, sender.getNickname(), targetNickname));
-	// 	return ;
-	// }
-
-	// _channels[channelName].kick(*target, sender, reason);
+	if (_channels.find(channelName) == _channels.end())
+	{
+		sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
+		return ;
+	}
+	for (size_t i = 0; i < targetsNickname.size(); ++i)
+	{
+		User* target = _server.getUserByNickname(targetsNickname[i]);
+		if (!target)
+		{
+			sender.pendingPush(reply(ERR_NOSUCHNICK, sender.getNickname(), targetsNickname[i]));
+			continue ;
+		}
+		_channels[channelName].kick(*target, sender, reason);
+	}
 }
 
 void ChannelManager::modeManager(User &sender, const Message &msg)
 {
-	(void)sender;
-	(void)msg;
-	// const std::string&	channelName = 
-	// const std::string&	modes =
-	// const std::string&	optionalParam =
+	const std::string& channelName = msg.getParamsVec()[0];
+	const std::string&	pswd = msg.getModeKey();
+	const std::string&	limit = msg.getModeLimit();
+	const std::string&	nickname = msg.getModeNick();
+	User* target = _server.getUserByNickname(nickname);
 
-	// if (_channels.find(channelName) == _channels.end())
-	// {
-	// 	sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
-	// 	return ;
-	// }
-
-	// _channels[channelName].setMode(modes, sender, optionalParam, "", NULL);
+	if (_channels.find(channelName) == _channels.end())
+	{
+		sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
+		return ;
+	}
+	if (target == NULL && !nickname.empty()) //ERR_NOSUCHNICK
+	{
+		sender.pendingPush(reply(ERR_NOSUCHNICK, sender.getNickname(), nickname));
+		return ;
+	}
+	if (msg.getParamsVec().size() > 1)
+	{
+		const std::string&	modes = msg.getParamsVec()[1];
+		_channels[channelName].setMode(modes, sender, pswd, limit, target);
+	}
+	else
+		_channels[channelName].getModes();
 	
 }
 
 void ChannelManager::topicManager(User &sender, const Message &msg)
 {
-	// std::string channelName = msg.getParams();
-	const std::string&	channelName = msg.getParams(); //
-	const std::string&	newTopic = msg.getTrailing(); //
+	const std::string& channelName = msg.getParamsVec()[0];
+	const std::string&	newTopic = msg.getTrailing();
 
 	if (_channels.find(channelName) == _channels.end())
 	{
 		sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
-		// std::cout << ":server 403 " << sender.getNickname() << " " << channelName << " :No such channel" << std::endl;
 		return ;
 	}
-	// if (msg.getTrailing().empty())
-	if (newTopic.empty()) //
+	if (newTopic.empty())
 		_channels[channelName].getTopic(sender);
-	// else if (msg.getTrailing().compare(":") == 0)
-	else if (newTopic.compare(":") == 0) //
+	else if (newTopic.compare(":") == 0)
 		_channels[channelName].setTopic(sender, "");
 	else
-		_channels[channelName].setTopic(sender, newTopic); //
-		// _channels[channelName].setTopic(sender, msg.getTrailing());
+		_channels[channelName].setTopic(sender, newTopic);
 }
 
 void ChannelManager::quitManager(User &sender)
@@ -155,52 +145,50 @@ void ChannelManager::quitManager(User &sender)
 	(void)sender;
 	//boucle dans tout les channels pour chercher si le sender est dedans car deconnexion = part des channels.
 	//voir pour mettre un message PART different genre "disconnected", avec RPL_QUIT ou autre ou ajouté parametre à removeMembers
+	//voir pour creer dans channel fonction quit sinon et faire un message different
 
-	// for (std::map<std::string, Channel>::iterator it = _channels.begin(); it != _channels.end();)
-	// {
-	// 	it->second.removeMember(sender, "Disconnected");
-	// 	if (it->second.getMembers().empty())
-	// 	{
-	// 		it = _channels.erase(it);
-	// 	}
-	// 	else
-	// 		++it;
-	// }
+	for (std::map<std::string, Channel>::iterator it = _channels.begin(); it != _channels.end();)
+	{
+		if (it->second.getMembers().find(&sender) != it->second.getMembers().end())
+			it->second.removeMember(sender, "Disconnected");
+		if (it->second.getMembers().empty())
+		{
+			it = _channels.erase(it);
+		}
+		else
+			++it;
+	}
 }
 
 // sending messages to a channel
-void ChannelManager::privmsgManager(User &sender, const Message& msg)
+void ChannelManager::privmsgManager(User &sender, const std::string &channelName, const std::string &message)
 {
-	const std::string&	channelName = msg.getParams();
-	const std::string&	message = msg.getTrailing();
-
 	if (_channels.find(channelName) == _channels.end())
 	{
 		sender.pendingPush(reply(ERR_NOSUCHCHANNEL, sender.getNickname(), channelName));
 		return ;
 	}
-
-	_channels[channelName].broadcast(sender, message); //?
+	_channels[channelName].broadcast(sender, message);
 }
 
 
 // for direct channel replies
-void ChannelManager::privmsgManager(User &sender, const std::string &chan, const std::string &reply)
-{
-	// if (_channels.count(chan) == 0)
-	// 	return sender.pendingPush("INSERT REPLY 403 HERE");
+// void ChannelManager::privmsgManager(User &sender, const std::string &chan, const std::string &reply)
+// {
+// 	// if (_channels.count(chan) == 0)
+// 	// 	return sender.pendingPush("INSERT REPLY 403 HERE");
 
-	if (_channels.find(chan) == _channels.end())
-	{
-		sender.pendingPush(::reply(ERR_NOSUCHCHANNEL, sender.getNickname(), chan)); // "::" used to avoid conflict with function param "&reply"
-		return ;
-	}
+// 	if (_channels.find(chan) == _channels.end())
+// 	{
+// 		sender.pendingPush(::reply(ERR_NOSUCHCHANNEL, sender.getNickname(), chan)); // "::" used to avoid conflict with function param "&reply"
+// 		return ;
+// 	}
 
-	// const std::map<User*,bool> members(_channels[chan].getMembers());
-	const std::map<User*, bool>& members = _channels[chan].getMembers();
-	for (std::map<User*, bool>::const_iterator it = members.begin(); it != members.end(); ++it)
-	{
-		if (it->first != &sender)
-			it->first->pendingPush(reply);
-	}
-}
+// 	// const std::map<User*,bool> members(_channels[chan].getMembers());
+// 	const std::map<User*, bool>& members = _channels[chan].getMembers();
+// 	for (std::map<User*, bool>::const_iterator it = members.begin(); it != members.end(); ++it)
+// 	{
+// 		if (it->first != &sender)
+// 			it->first->pendingPush(reply);
+// 	}
+// }
